98c2684692c2ef02919bde90e2c07c4d56335d12
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-02-17 23:57:49 +0300

    Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from "./components/User/User";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/users/:userId" element={<User/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Post/Post.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+
+import CommentIcon from "@mui/icons-material/Comment";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Post(props) {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+  const { userId, userName, title, text } = props;
+  const [liked, setLiked] = React.useState(false);
+
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+
+  return (
+    <Card sx={{ Width: 600, mb: 5, textAlign: "left" }}>
+      <CardHeader
+        avatar={
+          <Link className="link" to={{ pathname: "/users/" + userId }}>
+            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+              {userName.charAt(0).toUpperCase()}
+            </Avatar>
+          </Link>
+        }
+        title={title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+          {text}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites" onClick={handleLike}>
+          <FavoriteIcon style={liked ? { color: "red" } : null} />
+        </IconButton>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <CommentIcon />
+        </ExpandMore>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and
+            set aside for 10 minutes.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
